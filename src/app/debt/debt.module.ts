2bb57ded4582a6e692f2ead13bc333455814cf8e
Sarumathi Sugumar <sarumathi.sugumar@aspiresys.com>
2025-11-05 15:36:30 +0530

    Addressed route based PR comments

--- src/app/debt/debt.module.ts
+++ src/app/debt/debt.module.ts
@@ -12,7 +12,6 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild([{ path: '', component: DebtComponent }]),
     DebtRoutingModule
   ]
 })
