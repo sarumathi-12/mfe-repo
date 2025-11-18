989bf3d941e73bd699d435d0f4801ca842ffd5fb
Sarumathi Sugumar <sarumathi.sugumar@aspiresys.com>
2025-11-18 19:03:32 +0530

    unit-testcase-implementation

--- src/app/debt/header/header.component.spec.ts
+++ src/app/debt/header/header.component.spec.ts
@@ -1,21 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  //  TEST 1: Component Creates
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+ //  TEST 2: Should render name
+  it('should display the name', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const nameText = compiled.querySelector('.logo-text h2');
+
+    expect(nameText?.textContent?.trim()).toBe('Sarumathi Sugumar');
+  });
+
 });
